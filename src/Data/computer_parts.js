84c3a8ef1341f2a2a0508a4b871f16035036889e
primalkz <gaurangjha999@gmail.com>
2023-08-04 17:40:38 +0530

    data files

---
+++ src/Data/computer_parts.js
@@ -0,0 +1,202 @@
+export const computer_parts = [
+    {
+        "imageUrl": "https://www.mymbt.net/pub/media/catalog/product/cache/8a59274a72317ffdf6ff3193238efca6/i/n/intel_i9-11900k_11th_generation_processor_16mb_cache_5.20_ghz_3_.png",
+        "brand": "Intel",
+        "title": "Intel Core i9-11900K",
+        "color": "Silver",
+        "discountedPrice": 489,
+        "price": 599,
+        "discountPersent": 18,
+        "size": [
+            {
+                "name": "Standard",
+                "quantity": 100
+            }
+        ],
+        "quantity": 100,
+        "topLevelCategory": "Electronics",
+        "secondLevelCategory": "Computer Parts",
+        "thirdLevelCategory": "CPU",
+        "description": "The Intel Core i9-11900K is a powerful and efficient processor designed for gaming and high-performance computing tasks."
+    },
+    {
+        "imageUrl": "https://www.mymbt.net/pub/media/catalog/product/cache/8a59274a72317ffdf6ff3193238efca6/z/o/zotac_geforce_gaming_rtx_3080_trinity_oc_10gb_gddr6_graphics_card_1_.png",
+        "brand": "NVIDIA",
+        "title": "NVIDIA GeForce RTX 3080",
+        "color": "Black",
+        "discountedPrice": 899,
+        "price": 999,
+        "discountPersent": 10,
+        "size": [
+            {
+                "name": "Standard",
+                "quantity": 50
+            }
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Electronics",
+        "secondLevelCategory": "Computer Parts",
+        "thirdLevelCategory": "GPU",
+        "description": "The NVIDIA GeForce RTX 3080 is a high-end graphics card known for its exceptional gaming performance and real-time ray tracing capabilities."
+    },
+    {
+        "imageUrl": "https://www.mymbt.net/pub/media/catalog/product/cache/8a59274a72317ffdf6ff3193238efca6/c/o/corsair_vengeance_lpx_16gb_16gbx1_3200mhz_ddr4_ram_01.png",
+        "brand": "Corsair",
+        "title": "Corsair Vengeance LPX 16GB DDR4 RAM",
+        "color": "Black",
+        "discountedPrice": 89,
+        "price": 109,
+        "discountPersent": 18,
+        "size": [
+            {
+                "name": "16GB",
+                "quantity": 100
+            }
+        ],
+        "quantity": 100,
+        "topLevelCategory": "Electronics",
+        "secondLevelCategory": "Computer Parts",
+        "thirdLevelCategory": "RAM",
+        "description": "The Corsair Vengeance LPX 16GB DDR4 RAM is a high-performance memory module designed to enhance your computer's multitasking capabilities."
+    },
+    {
+        "imageUrl": "https://www.mymbt.net/pub/media/catalog/product/cache/8a59274a72317ffdf6ff3193238efca6/a/s/asus_rog_strix_b760-i_gaming_wifi_mini-itx_intel_lga1700_ddr5_motherboard_1_.png",
+        "brand": "ASUS",
+        "title": "ASUS ROG Strix B550-F Gaming Motherboard",
+        "color": "Black",
+        "discountedPrice": 199,
+        "price": 249,
+        "discountPersent": 20,
+        "size": [
+            {
+                "name": "ATX",
+                "quantity": 50
+            }
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Electronics",
+        "secondLevelCategory": "Computer Parts",
+        "thirdLevelCategory": "Motherboard",
+        "description": "The ASUS ROG Strix B550-F Gaming is a feature-packed motherboard suitable for gaming enthusiasts and content creators."
+    },
+    {
+        "imageUrl": "https://bdmarketprice.com/wp-content/uploads/2020/11/Samsung-970-EVO-Plus.jpg",
+        "brand": "Samsung",
+        "title": "Samsung 970 EVO Plus 1TB NVMe SSD",
+        "color": "Black",
+        "discountedPrice": 169,
+        "price": 199,
+        "discountPersent": 15,
+        "size": [
+            {
+                "name": "1TB",
+                "quantity": 100
+            }
+        ],
+        "quantity": 100,
+        "topLevelCategory": "Electronics",
+        "secondLevelCategory": "Computer Parts",
+        "thirdLevelCategory": "Storage",
+        "description": "The Samsung 970 EVO Plus 1TB NVMe SSD offers fast data transfer speeds and ample storage capacity for your computing needs."
+    },
+    {
+        "imageUrl": "https://m.media-amazon.com/images/I/7163oT1ia3L._AC_UF1000,1000_QL80_.jpg",
+        "brand": "EVGA",
+        "title": "EVGA SuperNOVA 750 G3 Power Supply",
+        "color": "Black",
+        "discountedPrice": 129,
+        "price": 149,
+        "discountPersent": 13,
+        "size": [
+            {
+                "name": "750W",
+                "quantity": 50
+            }
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Electronics",
+        "secondLevelCategory": "Computer Parts",
+        "thirdLevelCategory": "Power Supply",
+        "description": "The EVGA SuperNOVA 750 G3 is a reliable and efficient power supply unit that delivers stable power to your computer components."
+    },
+    {
+        "imageUrl": "https://www.mymbt.net/pub/media/catalog/product/cache/8a59274a72317ffdf6ff3193238efca6/c/o/cooler_master_hyper_212_red_led_120mm_cpu_cooler_fan_1__1.png",
+        "brand": "Cooler Master",
+        "title": "Cooler Master Hyper 212 RGB CPU Cooler",
+        "color": "Silver",
+        "discountedPrice": 39,
+        "price": 49,
+        "discountPersent": 20,
+        "size": [
+            {
+                "name": "Standard",
+                "quantity": 100
+            }
+        ],
+        "quantity": 100,
+        "topLevelCategory": "Electronics",
+        "secondLevelCategory": "Computer Parts",
+        "thirdLevelCategory": "Cooling",
+        "description": "The Cooler Master Hyper 212 RGB CPU Cooler offers efficient cooling and adds a touch of customizable RGB lighting to your system."
+    },
+    {
+        "imageUrl": "https://m.media-amazon.com/images/I/510cWHKXlrL._AC_UF1000,1000_QL80_.jpg",
+        "brand": "NZXT",
+        "title": "NZXT H510 Compact ATX Mid-Tower Case",
+        "color": "White",
+        "discountedPrice": 79,
+        "price": 99,
+        "discountPersent": 20,
+        "size": [
+            {
+                "name": "ATX",
+                "quantity": 50
+            }
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Electronics",
+        "secondLevelCategory": "Computer Parts",
+        "thirdLevelCategory": "Case",
+        "description": "The NZXT H510 is a sleek and compact mid-tower case with ample space for your components and excellent cable management."
+    },
+    {
+        "imageUrl": "https://www.amd.com/system/files/2020-09/616656-amd-ryzen-7-5000-series-PIB-1260x709_0.png",
+        "brand": "AMD",
+        "title": "AMD Ryzen 7 5800X",
+        "color": "Black",
+        "discountedPrice": 379,
+        "price": 449,
+        "discountPersent": 16,
+        "size": [
+            {
+                "name": "Standard",
+                "quantity": 50
+            }
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Electronics",
+        "secondLevelCategory": "Computer Parts",
+        "thirdLevelCategory": "CPU",
+        "description": "The AMD Ryzen 7 5800X is a high-performance processor with 8 cores and 16 threads, perfect for content creation and gaming."
+    },
+    {
+        "imageUrl": "https://www.mymbt.net/pub/media/catalog/product/cache/8a59274a72317ffdf6ff3193238efca6/g/s/gskill_ripjaws_v_32gb_16gbx2_3600mhz_ddr4_ram_1_.png",
+        "brand": "G.Skill",
+        "title": "G.Skill Ripjaws V Series 32GB DDR4 RAM",
+        "color": "Black",
+        "discountedPrice": 159,
+        "price": 189,
+        "discountPersent": 16,
+        "size": [
+            {
+                "name": "32GB",
+                "quantity": 100
+            }
+        ],
+        "quantity": 100,
+        "topLevelCategory": "Electronics",
+        "secondLevelCategory": "Computer Parts",
+        "thirdLevelCategory": "RAM",
+        "description": "The G.Skill Ripjaws V Series 32GB DDR4 RAM provides high-speed and stable performance for demanding computing tasks."
+    }
+];
