e7868034483c59191fe68a9283b6a7ea9f41b902
primalkz <44187697+primalkz@users.noreply.github.com>
2023-08-05 14:38:15 +0530

    Add: Product page and product filtering

---
+++ src/customer/components/Product/ProductCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./ProductCard.css"
+
+const ProductCard = ({product}) => {
+    return (
+        <div className='productCard w-[14rem] m-3 transition-all cursor-pointer'>
+            <div className='h-[12rem]'>
+                <img className='object-cover object-top w-full h-full' src={product.imageUrl} alt=""/>
+            </div>
+            <div className='textPart bg-white p-3'>
+                <div className=''>
+                    <p className='font-bold opacity-60'>{product.brand}</p>
+                    <p>{product.title}</p>
+                </div>
+                <div className='flex item-center space-x-1 px-6'>
+                    <p className='font-semibold'>₹{product.discountedPrice}</p>
+                    <p className='font-sm line-through opacity-60'>₹{product.price}</p>
+                    <p className='text-green-600 font-semibold'>{product.discountPersent}% off</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard;
