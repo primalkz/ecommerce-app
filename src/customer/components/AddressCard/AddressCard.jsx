83c51192f87b96750aff51a269e264d88977cdc5
primalkz <gaurangjha999@gmail.com>
2023-08-08 22:30:03 +0530

    Add AddressCard page to display on CartItems page

---
+++ src/customer/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AddressCard = () => {
+  return (
+    <div>
+        <div className='space-y-3'>
+            <p className='font-semibold'>ABC Kumar</p>
+            <p>Noida, Sector 18, 2013xx</p>
+            <div className='space-y-1'>
+                <p className='font-semibold'>Phone Number</p>
+                <p>1234567890</p>
+            </div>
+        </div>
+    </div>
+  ) 
+}
+
+export default AddressCard
